bfbaba9ab9b3be107378f0afb7d2e1d51c18df18
Hitmanop127 <73530938+Hitmanop127@users.noreply.github.com>
2021-02-13 18:45:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var gameState=0;
+var database,playerCount,form,player,game;
+
+function setup() {
+createCanvas(400,400) ;
+database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+}
+
+function draw() {
+
+}
